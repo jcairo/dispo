bfc709ae816dc8d689459db78044b33073bde904
Jon Cairo <jon@Jons-MacBook-Pro.local>
2015-04-19 21:03:34 -0600

    More map data.

--- src/app/main/map.controller.js
+++ src/app/main/map.controller.js
@@ -8,6 +8,10 @@ angular.module('dispo')
         map = mapInstance;
     });
 
+    $scope.submit = function () {
+
+    };
+
     $scope.sites = mockData.sites;
     $scope.setPolygons = function () {
         $scope.polygons = [];
@@ -18,13 +22,31 @@ angular.module('dispo')
 
     $scope.panMap = function(lat, lon) {
         $timeout( function () {
-            map.setZoom(8);
+            map.setZoom(10);
         }, 250);
         $timeout( function () {
-            map.setZoom(7);
+            map.setZoom(9);
         }, 500);
         $timeout( function () {
-            map.panTo({lat: lat, lng:lon});
+            map.setZoom(8);
         }, 750);
+        $timeout( function () {
+            map.setZoom(7);
+        }, 1000);
+        $timeout( function () {
+            map.panTo({lat: lat, lng:lon});
+        }, 1250);
+        $timeout( function () {
+            map.setZoom(8)
+        }, 1500);
+        $timeout( function () {
+            map.setZoom(9)
+        }, 1750);
+        $timeout( function () {
+            map.setZoom(10)
+        }, 2000);
+        $timeout( function () {
+            map.setZoom(11)
+        }, 2000);
     };
 });
--- src/app/mock_data.service.js
+++ src/app/mock_data.service.js
@@ -45,6 +45,47 @@ angular.module('dispo')
                 ],
                 elevation: '539m',
                 area: '2.3km',
+            },
+            {
+                name: 'Farland',
+                location: 'West Alberta',
+                size: '5 Hectares',
+                polygon: [
+                    [54.3, -115.52],
+                    [54.28, -115.52],
+                    [54.28, -115.55],
+                    [54.3, -115.55],
+                ],
+                elevation: '500m',
+                area: '7km',
+            },
+            {
+                name: 'Marland',
+                location: 'East Alberta',
+                size: '1 Hectare',
+                polygon: [
+                    [56.53677225,  -115.86765289],
+                    [56.59201007,  -115.87177277],
+                    [56.59238813,  -115.79143524],
+                    [56.60108254,  -115.76808929],
+                    [56.54207252,  -115.76328278],
+                    [56.53677225,  -115.86765289]
+                ],
+                elevation: '745m',
+                area: '1km',
+            },
+            {
+                name: 'Shardland',
+                location: 'East Alberta',
+                size: '2 Hectares',
+                polygon: [
+                    [58.0720666, -118.37287903],
+                    [58.05535853, -118.35777283],
+                    [58.05390528, -118.30421448],
+                    [58.09384799, -118.30284119]
+                ],
+                elevation: '539m',
+                area: '2.3km',
             }
         ]
     };
